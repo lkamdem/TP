681c1f245e070940fe86cd4a0e0063f7a5ace43f
Leopold KAMDEM DJOKO <kamdemleo@yahoo.fr>
2020-03-20 15:43:59 +0100

    Ajout des commentaires

--- src/ExoNIO/Main.java
+++ src/ExoNIO/Main.java
@@ -13,23 +13,30 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         List<String> listeLignes = new ArrayList<>();
-        listeLignes.add("premiere ligne de ma chanson préféré, premier couplet");
-        listeLignes.add("seconde ligne de ma chanson préféré, premier couplet");
-        listeLignes.add("troisième ligne de ma chanson préféré, premier couplet");
-        listeLignes.add("refrain ma");
-        listeLignes.add("première ligne de ma chanson préféré, second couplet");
-        System.out.println(listeLignes);
+        listeLignes.add("premiere ligne de ma chanson préféré, premier couplet.");
+        listeLignes.add("seconde ligne de ma chanson préféré, premier couplet.");
+        listeLignes.add("troisième ligne de ma chanson préféré, premier couplet.");
+        listeLignes.add("refrain ma chanson préféré.");
+        listeLignes.add("première ligne de ma chanson préféré, second couplet.");
+//        System.out.println(listeLignes);
 
 
         Path monFichier =  Paths.get("texte.txt");
+//        Suppression du fichier JDD s'il existe déjà
         if (Files.exists(monFichier)) {
             suppressionFichier(monFichier);
         }
+//        création du fichier JDD
         creerFichier(monFichier);
+//        écriture dans le fichier JDD
         ecrireFichier(listeLignes, monFichier);
 
+//        Utilisation de la méthode compterMot, pour compter le nombre de mot dans le fichier
         System.out.println(compterMot(monFichier));
+
+//        Création d'une liste de String qui servira pour l'écriture dans le fichier contant les mots et leur occurence
         List<String> ligneEcriture = new ArrayList<>();
+
         Map<String, Integer> dico = compterMot(monFichier);
         int totalMots = 0;
         for (String key : dico.keySet()) {
@@ -37,12 +44,13 @@ public class Main {
             totalMots += dico.get(key);
         }
         ligneEcriture.add("Total des mots = " + totalMots);
-        Path secondFichier =  Paths.get("occurence.txt");
-        if (Files.exists(secondFichier)) {
-            suppressionFichier(secondFichier);
+
+        Path fichierOccurence =  Paths.get("occurence.txt");
+        if (Files.exists(fichierOccurence)) {
+            suppressionFichier(fichierOccurence);
         }
-        creerFichier(secondFichier);
-        ecrireFichier(ligneEcriture, secondFichier);
+        creerFichier(fichierOccurence);
+        ecrireFichier(ligneEcriture, fichierOccurence);
 
         System.out.println(motPlusPresent(monFichier));
     }
@@ -70,17 +78,23 @@ public class Main {
             e.printStackTrace();
         }
     }
-
+//Methode qui retourne un dictionnaire avec la liste des mot et leur nombre d'occurence
     static Map<String, Integer> compterMot(Path fichier) {
+//        création du dictionnaire qui sera retourné
         Map<String,Integer> dicoMot = new HashMap<>();
+//        Création d'une liste pour contenir le résultat de la lecture
         List<String> lecture = new ArrayList<>();
         try {
             lecture = Files.readAllLines(fichier);
         } catch (IOException e) {
             e.printStackTrace();
         }
+//
         for (String ligne : lecture) {
             for (String mot : ligne.split(" ")) {
+                if (mot.endsWith(",") || (mot.endsWith("."))) {
+                    mot=mot.substring(0,mot.length()-1);
+                }
                 int compteur = getCompteur(mot, dicoMot);
                 dicoMot.put(mot,compteur+1);
             }
